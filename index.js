dc2a6675a414c8bb0e7a1b3577c65167c8853652
Taimur Shah <tshah@scu.edu>
2018-10-22 19:03:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ var recipes = {}
 function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign({}, obj, {[key]:value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object
+}

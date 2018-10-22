5e6ce5b62e680949087af2c60c436a11340484ac
Taimur Shah <tshah@scu.edu>
2018-10-22 18:25:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var recipes = {}
 
-function updateObjectWithKeyAndValue(obj, key, value) {
-  obj[key] = value;
-  return obj
-}
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-  return Object.assign({key: value}, obj )
+  return Object.assign({}, object, {[key]:value})
 }

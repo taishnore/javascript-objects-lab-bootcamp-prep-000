852f84cd805a7ce2ea1184958aa30c460df82fd0
Taimur Shah <tshah@scu.edu>
2018-10-22 19:18:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, )
+  var newObj = Object.assign({}, object);
+  delete newObj.key;
+  return 
 }
